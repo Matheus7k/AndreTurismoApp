1fc052b20f72088ec0007ea7a5f2c43dbe8b4f48
matheus7k <06matheus.oliveira@gmail.com>
2023-05-02 02:52:02 -0300

    Criado DTO

---
+++ AndreTurismoApp.Models/DTOs/CreatePackageDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreTurismoApp.Models.DTOs
+{
+    public class CreatePackageDTO
+    {
+        public int HotelId { get; set; }
+        public int TicketId { get; set; }
+        public int CustomerId { get; set; }
+        public double Price { get; set; }
+    }
+}
