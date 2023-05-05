3e08d0f051de48e4b89004af74be16b7404e26ea
matheus7k <06matheus.oliveira@gmail.com>
2023-05-05 10:06:45 -0300

    Criado microsserviço para salvar no banco de dados

---
+++ AndreTurismoApp.DBController/Controllers/DBCustomersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AndreTurismoApp.DBController.Data;
+using AndreTurismoApp.Models;
+
+namespace AndreTurismoApp.DBController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DBCustomersController : ControllerBase
+    {
+        private readonly AndreTurismoAppDBControllerContext _context;
+
+        public DBCustomersController(AndreTurismoAppDBControllerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            if (_context.Customer == null)
+            {
+                return NotFound();
+            }
+            return await _context.Customer.Include(c => c.Address.City).ToListAsync();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            if (_context.Customer == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customer.Include(c => c.Address.City).Where(c => c.Id == id).FirstAsync();
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            if (_context.Customer == null)
+            {
+                return Problem("Entity set 'AndreTurismoAppDBControllerContext.Customer'  is null.");
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+            _context.Customer.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            if (_context.Customer == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customer.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return (_context.Customer?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AndreTurismoApp.DBController/Data/AndreTurismoAppDBControllerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AndreTurismoApp.Models;
+
+namespace AndreTurismoApp.DBController.Data
+{
+    public class AndreTurismoAppDBControllerContext : DbContext
+    {
+        public AndreTurismoAppDBControllerContext (DbContextOptions<AndreTurismoAppDBControllerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AndreTurismoApp.Models.Customer> Customer { get; set; } = default!;
+    }
+}
